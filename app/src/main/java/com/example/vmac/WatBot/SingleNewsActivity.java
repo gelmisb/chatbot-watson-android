33927b3bfbc590d96527323ef106159e6c247c62
gelmisb <gelmisb@gmail.com>
2018-02-19 19:31:06 +0000

    App news updated

--- app/src/main/java/com/example/vmac/WatBot/SingleNewsActivity.java
+++ app/src/main/java/com/example/vmac/WatBot/SingleNewsActivity.java
@@ -1,14 +1,19 @@
 package com.example.vmac.WatBot;
 
 import android.app.ProgressDialog;
+import android.content.ComponentCallbacks2;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -35,7 +40,7 @@ public class SingleNewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_news);
 
-        Article article = (Article)getIntent().getSerializableExtra("Article");
+        final Article article = (Article)getIntent().getSerializableExtra("Article");
 
         Log.d("ARTICLE BITH", article.getTitle());
 
@@ -43,16 +48,27 @@ public class SingleNewsActivity extends AppCompatActivity {
         TextView aDescription = (TextView)findViewById(R.id.description);
         TextView aAuthor = (TextView)findViewById(R.id.author);
         TextView aDate = (TextView)findViewById(R.id.date);
+        ImageView aLink = (ImageView)findViewById(R.id.read);
 
         aTitle.setText(article.getTitle());
         aDescription.setText(article.getContent());
         aAuthor.setText(article.getAuthor());
         aDate.setText(article.getDate());
 
+        aLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(article.getURL()));
+                startActivity(browserIntent);
+
+            }
+        });
+
 
         String imageUrl = article.getImageURL();
         new GetImage().execute(imageUrl,null,null);
-
+    onTrimMemory(TRIM_MEMORY_COMPLETE);
     }
 
 
@@ -64,7 +80,6 @@ public class SingleNewsActivity extends AppCompatActivity {
         protected String doInBackground(String... urls) {
 
             try{
-                System.out.println(urls[0] + "THIS IS THE URL WE'RE USING");
                 url = new URL(urls[0]);
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -86,6 +101,69 @@ public class SingleNewsActivity extends AppCompatActivity {
         protected void onPostExecute(String image){
             iv = (ImageView) findViewById(R.id.image);
             iv.setImageDrawable(d);
+        }
+
+
+    }
+
+
+    /**
+     * Release memory when the UI becomes hidden or when system resources become low.
+     * @param level the memory-related event that was raised.
+     */
+    public void onTrimMemory(int level) {
+
+        // Determine which lifecycle or system event was raised.
+        switch (level) {
+
+            case ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN:
+
+                /*
+                   Release any UI objects that currently hold memory.
+
+                   The user interface has moved to the background.
+                */
+
+                break;
+
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_MODERATE:
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW:
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_CRITICAL:
+
+                /*
+                   Release any memory that your app doesn't need to run.
+
+                   The device is running low on memory while the app is running.
+                   The event raised indicates the severity of the memory-related event.
+                   If the event is TRIM_MEMORY_RUNNING_CRITICAL, then the system will
+                   begin killing background processes.
+                */
+
+                break;
+
+            case ComponentCallbacks2.TRIM_MEMORY_BACKGROUND:
+            case ComponentCallbacks2.TRIM_MEMORY_MODERATE:
+            case ComponentCallbacks2.TRIM_MEMORY_COMPLETE:
+
+                /*
+                   Release as much memory as the process can.
+
+                   The app is on the LRU list and the system is running low on memory.
+                   The event raised indicates where the app sits within the LRU list.
+                   If the event is TRIM_MEMORY_COMPLETE, the process will be one of
+                   the first to be terminated.
+                */
+
+                break;
+
+            default:
+                /*
+                  Release any non-critical data structures.
+
+                  The app received an unrecognized memory level value
+                  from the system. Treat this as a generic low-memory message.
+                */
+                break;
         }
     }
 }
